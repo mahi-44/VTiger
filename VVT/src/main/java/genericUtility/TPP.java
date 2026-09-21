25573811fefdffce9c126594378db82413f70518
User <User@DESKTOP-L7RC180>
2026-09-21 18:15:30 +0530

    Created 1 annotation in TPP

--- VVT/src/main/java/genericUtility/TPP.java
+++ VVT/src/main/java/genericUtility/TPP.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class TPP {
-	
+
 	public TPP() {
-		
+
 	}
 
 	@FindBy(linkText = "Organization")
@@ -15,12 +15,16 @@ public class TPP {
 	@FindBy(linkText = "Campaigns")
 	private WebElement campaignsLink;
 
+	@FindBy(linkText = "Sign Out")
+	private WebElement signOutLink;
 
+	public WebElement getCampaignsLink() {
+		return campaignsLink;
+	}
 
 	public WebElement getOrganization() {
 		// TODO Auto-generated method stub
 		return contactsLink;
-	
 
 	}
 
